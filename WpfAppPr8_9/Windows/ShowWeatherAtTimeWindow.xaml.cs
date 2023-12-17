b535a12beae9130df445a163e566d9587903c203
KlopovaMM <is24klopovamm@artcollege.ru>
2023-12-17 17:19:44 +0300

    Добавьте файлы проекта.

---
+++ WpfAppPr8_9/Windows/ShowWeatherAtTimeWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using WpfAppPr8_9.Classes;
+using WpfAppPr8_9.DataModel;
+using WpfAppPr8_9.CustomControls;
+
+namespace WpfAppPr8_9.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowWeatherAtTimeWindow.xaml
+    /// </summary>
+    public partial class ShowWeatherAtTimeWindow : Window
+    {
+        private Prognosis prognosis;
+        public ShowWeatherAtTimeWindow(Prognosis prog)
+        {
+            InitializeComponent();
+            prognosis = prog;
+            labelContent.Content += " " + prognosis.Date.Value.ToShortDateString();
+            LoadDatalWrapPanel(prog.Time);
+        }
+        private void LoadDatalWrapPanel(string info)
+        {
+            List<ComplexWeatherInfo> infos;
+                      
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<ComplexWeatherInfo>));
+            using (StringReader reader = new StringReader(info))
+            {
+                infos = xmlSerializer.Deserialize(reader) as List<ComplexWeatherInfo>;
+            }
+
+            infos.ForEach(item => wrapPanelWeather.Children.Add(new WeatherCustomControl(item)));
+
+        }
+
+
+    }
+}
---
+++ WpfAppPr8_9/Windows/ShowWeatherWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppPr8_9.DataModel;
+using WpfAppPr8_9.CustomControls;
+
+
+namespace WpfAppPr8_9.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowWeatherWindow.xaml
+    /// </summary>
+    public partial class ShowWeatherWindow : Window
+    {
+        public ShowWeatherWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        private void LoadData() =>
+            new Model1().Prognosis.ToList().ForEach(item =>
+            wrapPanelWeather.Children.Add(new PrognosisWeather(item)));
+
+        private void buttonBack_Click(object sender, RoutedEventArgs e) => Close();
+    }
+}
